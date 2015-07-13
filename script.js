ddea3a661ca098a08554eb06941c626a56e6e6eb
Mostafa Habib <MostafaSHabib@gmail.com>
2015-07-13 17:52:51 -0400

    added Fisher-Yates algo

--- script.js
+++ script.js
@@ -16,25 +16,65 @@ var questions = [
 	}
 ];
 
+//=========================================Fisher-Yates Algorithun 
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+shuffle(questions);
+
+
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+// console.log(getRandomInt(1, 3));
+
+
+
+// 						============================================reset 
 $('#resetButton').on('click' , function(){
 	points = 0;
 	alert('score reset you are now at '+points)
 	$('.question').html(points);
 });
 
+
+
+
 $('#begin').on('click' , function(){
-	for (var i = 0 ; i < questions.length ; i++) {
- 		// -------------------------------------trying to use innerHTML on the p tag 
- 		// $('p').text(questions[i].question);
- 		// $('submitButton').on('click' , function(){
- 		// 	if ($('input').val() === questions[i].answer){
- 		// 		alert('this works');
- 		// 	} 
- 		// })
+	//var choice = getRandomInt(1, 3);
+		for (var i = 0 ; i < questions.length ; i++) {
+
+ 				// -------------------------------------trying to use innerHTML on the p tag 
+ 				// $('p').text(questions[i].question);
+ 				// $('submitButton').on('click' , function(){
+ 				// 	if ($('input').val() === questions[i].answer){
+ 				// 		alert('this works');
+ 				// 	} 
+ 				// })
+
+
 		var answerDizzle = prompt(questions[i].question+'?')
 			if (answerDizzle === questions[i].answer) {
 				points++;
 				alert('wow you are so smart, currently you have a score of '+points);
+				//questions.splice(i, 1);
 				$('.question').html(points);							
 			}
 			else {
@@ -42,7 +82,7 @@ $('#begin').on('click' , function(){
 				alert('WRONG!! you now have a score of '+points);
 				$('.question').html(points);
 			}
-	}
+		}
 
 });
 
