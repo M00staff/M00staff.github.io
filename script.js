23a7190e381de232d1f9ffe14d1376211f651b9b
Mostafa Habib <MostafaSHabib@gmail.com>
2015-08-23 17:57:34 -0400

    added project 2 and added transition back CSS

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+//$('#project1').fadeIn(2000);
+
 $('#project1').hover(function() {
-	$(this).text("A Simple Trivia Game")
+	$(this).text("A simple trivia game using Javascript/JQuery, HTML and CSS")
 	}
 ,
 function(){
 	$(this).text('Project 1')
+	}
+);
+
+
+$('#project2').hover(function() {
+	$(this).text("A low carb cook book application built with Ruby on Rails and deployed online with Heroku")
+	}
+,
+function(){
+	$(this).text('Project 2')
 	}
 );
