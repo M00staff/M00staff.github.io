06fbb9232ce4070a259104aba1b717f79c957476
Mostafa Habib <MostafaSHabib@gmail.com>
2015-08-27 15:19:51 -0400

    added project 3

--- script.js
+++ script.js
@@ -20,10 +20,19 @@ function(){
 );
 
 $('#projectUX').hover(function() {
-	$(this).text("A group project of 4 Web Dev students and 2 UX Design students - an app that lets students submit comfort/completeness about a lesson after its been taught")
+	$(this).text("A group project of 4 Web Dev students and 2 UX Design students - an app written with Ruby on Rails that lets students submit comfort/completeness about a lesson after its been taught")
 	}
 ,
 function(){
 	$(this).text('UX collaboration')
+	}
+);
+
+$('#project3').hover(function() {
+	$(this).text("4 person group project written with an Express/Node backend.  The app utilizes Spotify's Echo Nest API as well as the Bands in Town API")
+	}
+,
+function(){
+	$(this).text('Project4')
 	}
 );
