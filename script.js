50189884329200927bc73dbe5b282dc2ffe73bc7
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-01 22:29:56 -0400

    styling

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $('#project1').hover(function() {
 	}
 ,
 function(){
-	$(this).text('Project 1')
+	$(this).text('Project 1 - Trivia')
 	}
 );
 
@@ -15,24 +15,24 @@ $('#project2').hover(function() {
 	}
 ,
 function(){
-	$(this).text('Project 2')
+	$(this).text('Project 2 - Keto Cook Book')
 	}
 );
 
 $('#projectUX').hover(function() {
-	$(this).text("A group project of 4 Web Dev students and 2 UX Design students - an app written with Ruby on Rails that lets students submit comfort/completeness about a lesson after its been taught")
+	$(this).text("Group project of 4 Dev students and 2 UX Design students - an app written with Ruby on Rails that lets students submit comfort/completeness about a lesson after its been taught")
 	}
 ,
 function(){
-	$(this).text('UX collaboration')
+	$(this).text('UX collaboration - The GA Anonymizer')
 	}
 );
 
 $('#project3').hover(function() {
-	$(this).text("4 person group project written with an Express/Node backend.  The app utilizes Spotify's Echo Nest API as well as the Bands in Town API")
+	$(this).text("Group project written with an Express/Node backend.  The app utilizes Spotify's Echo Nest API as well as BandsinTown's API")
 	}
 ,
 function(){
-	$(this).text('Project4')
+	$(this).text('Project 4 - Playlisr')
 	}
 );
